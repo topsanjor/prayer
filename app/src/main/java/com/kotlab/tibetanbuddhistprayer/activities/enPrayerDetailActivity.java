7e8c8d925636f3a75b28c91e85949548fc1c8962
topsanjor <topjor2014>
2017-12-05 02:39:38 +0530

    add title and body ...

--- app/src/main/java/com/kotlab/tibetanbuddhistprayer/activities/enPrayerDetailActivity.java
+++ app/src/main/java/com/kotlab/tibetanbuddhistprayer/activities/enPrayerDetailActivity.java
@@ -5,20 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.TextView;
 
 
 import com.kotlab.tibetanbuddhistprayer.R;
 import com.kotlab.tibetanbuddhistprayer.model.EnglishData;
 
+import org.w3c.dom.Text;
+
 public class enPrayerDetailActivity extends AppCompatActivity {
     Toolbar toolbar;
     private static final String TAG ="PrayerDetails";
+    private TextView txttitle, txtbody;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_en_prayer_detail);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        txttitle = (TextView) findViewById(R.id.entxttitle);
+        txtbody = (TextView) findViewById(R.id.entxtbody);
 
      try {
 
@@ -26,6 +32,8 @@ public class enPrayerDetailActivity extends AppCompatActivity {
 
          String title = englishData.getTitle();
          String body = englishData.getBody();
+         txttitle.setText(title);
+         txtbody.setText(body);
 
          Log.d(TAG,"title ="+title+" and body ="+body);
      }catch (Exception ex) {
